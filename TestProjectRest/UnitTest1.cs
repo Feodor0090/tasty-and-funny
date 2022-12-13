4da07af377fdb87f4854e5fb3e90ee193d143624
ansel <79257300125@ya.ru>
2022-12-13 11:30:42 +0300

    Переименовал тесты

--- TestProjectRest/UnitTest1.cs
+++ TestProjectRest/UnitTest1.cs
@@ -13,14 +13,14 @@ namespace TestProjectRest
         }
 
         [Test]
-        public void Test1()
+        public void ItemCreation()
         {
             Item item = new Item();
             int count =0;
             Assert.AreEqual(count, item.CountPriceByAmount(5));
         }
         [Test]
-        public void Test2()
+        public void DecrAmount()
         {
             Item item = new Item();
             OrderItem order = new OrderItem(item, 5);
@@ -29,22 +29,13 @@ namespace TestProjectRest
             Assert.AreEqual(count, order.Amount);
         }
         [Test]
-        public void Test3()
+        public void IncrAmount()
         {
             Item item = new Item();
             OrderItem order = new OrderItem(item, 5);
             int count = 6;
             order.IncAmount();
             Assert.AreEqual(count, order.Amount);
-        }
-        [Test]
-        public void Test4()
-        {
-            Item item = new Item();
-            OrderItem order = new OrderItem(item, 5);
-            int count =0 ;
-            order.IncAmount();
-            Assert.AreEqual(count, order.Price);
         }
     }
 }
